3e204bdb35b48d759ea7c172675b3ee0c69a99e7
kouwa stephane <kksasteph888@gmail.com>
2022-08-29 14:56:29 +0200

    add route to download themeé é

--- src/Controller/ExportImportEntitiesController.php
+++ src/Controller/ExportImportEntitiesController.php
@@ -6,11 +6,14 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\export_import_entities\Services\ExportEntities;
 use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\Response;
+use Drupal\domain\DomainNegotiator;
 
 /**
  * Returns responses for Export Import Entities routes.
  */
 class ExportImportEntitiesController extends ControllerBase {
+  protected $currentDomaine;
 
   /**
    *
@@ -18,12 +21,13 @@ class ExportImportEntitiesController extends ControllerBase {
    */
   protected $ExportEntities;
 
-  function __construct(ExportEntities $ExportEntities) {
+  function __construct(ExportEntities $ExportEntities, DomainNegotiator $DomainNegotiator) {
     $this->ExportEntities = $ExportEntities;
+    $this->currentDomaine = $DomainNegotiator->getActiveDomain();
   }
 
   static function create(ContainerInterface $container) {
-    return new static($container->get('export_import_entities.export.entites'));
+    return new static($container->get('export_import_entities.export.entites'), $container->get('domain.negotiator'));
   }
 
   /**
@@ -37,6 +41,18 @@ class ExportImportEntitiesController extends ControllerBase {
       '#markup' => $this->t('It works! ..')
     ];
     return $build;
+  }
+
+  public function DownloadSiteZip() {
+    $pt = explode('/web', DRUPAL_ROOT);
+    $baseZip = $pt[0] . '/sites_exports/zips/';
+    $path = $baseZip . $this->currentDomaine->id();
+    $response = new Response();
+    // $response->headers->set('Content-Type',
+    // 'application/zip,application/octet-stream');
+    $response->headers->set('Content-Type', 'application/zip');
+    $response->setContent(file_get_contents($path));
+    return $response;
   }
 
 }
