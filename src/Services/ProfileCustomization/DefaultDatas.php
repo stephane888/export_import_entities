53a9ac6d6dd6f8e7b3d6719ccf026ee6ba7adee0
kouwa stephane <kksasteph888@gmail.com>
2025-03-09 16:27:44 +0100

    add module drag_and_drop_files

--- src/Services/ProfileCustomization/DefaultDatas.php
+++ src/Services/ProfileCustomization/DefaultDatas.php
@@ -99,7 +99,8 @@ class DefaultDatas extends ControllerBase {
       'hbktemplateuser',
       'manage_module_config',
       'managepackvhsost',
-      'wb_commerce'
+      'wb_commerce',
+      'drag_and_drop_files'
     ];
   }
   
